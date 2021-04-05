2ec2f6ea9f922474d9b37125cc2bbce4c147e5d8
IvanyukStas <aswd25@yandex.ru>
2021-04-05 19:19:07 +0300

    добавил тег  в конфиг

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 channel_id = os.getenv('channel_id')
-title_teg = os.getenv('title_teg')
+title_teg = os.getenv('title_teg') or 'wants-card__left'
 
 #если слова есть в заголовке, то не парсить
 break_word = ['1с', 'верстка', 'android', 'ios', 'yii', 'php', 'java', 'bitrix 1c',
-              'bitrix', 'wp', 'Битрикс', 'Word press', 'Wordpress']
+              'bitrix', 'wp', 'Битрикс', 'Word press', 'Wordpress', 'Laravel']
 
 #время через которое бот заходит на сайт и парсит инфу
-time_interval = 600
+time_interval = 1200
 
 
