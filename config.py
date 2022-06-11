ac4ba0a421863421796beabb3c04ca3a9b25a738
aswd25@yandex.ru <Kadabrov1>
2022-06-11 14:26:32 +0300

    kwork parser v3

--- config.py
+++ config.py
@@ -1,16 +1,36 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
-channel_id = os.getenv('channel_id')
-title_teg = os.getenv('title_teg') or 'wants-card__left'
-
-#если слова есть в заголовке, то не парсить
-break_word = ['1с', 'верстка', 'android', 'ios', 'yii', 'php', 'java', 'bitrix 1c',
-              'bitrix', 'wp', 'Битрикс', 'Word press', 'Wordpress', 'Laravel']
-
-#время через которое бот заходит на сайт и парсит инфу
-time_interval = 1200
 
 
+class ConfigGeneral:
+    TOKEN = os.getenv("TOKEN")
+    CHANNEL_ID = os.getenv("channel_id")
+    # время через которое бот заходит на сайт и парсит инфу
+    TIME_INTERVAL = 1200
+
+
+class ConfigKwork:
+    TITLE_TAG = os.getenv("title_teg") or "wants-card__left"
+
+
+class ConfigFilter:
+    # если слова есть в заголовке, то не парсить
+    BREAK_WORD = [
+        "1с",
+        "верстка",
+        "android",
+        "ios",
+        "yii",
+        "php",
+        "java",
+        "bitrix 1c",
+        "bitrix",
+        "wp",
+        "Битрикс",
+        "Word press",
+        "Wordpress",
+        "Laravel",
+    ]
--- parser_vse_pesni.py
+++ parser_vse_pesni.py
@@ -6,63 +6,66 @@ import time
 import requests
 from bs4 import BeautifulSoup
 import config
+from config import ConfigGeneral, ConfigFilter
 
 
 def open_input_file(input_file) -> list:
     url = []
-    if not os.path.exists('output.csv'):
-       f = open('output.csv', 'w')
-       f.close()
-    with open(input_file, 'r', encoding="cp1251", errors='ignore') as file:
-        reader = csv.reader(file, delimiter=';',
-                            quoting=csv.QUOTE_MINIMAL)
+    if not os.path.exists("output.csv"):
+        f = open("output.csv", "w")
+        f.close()
+    with open(input_file, "r", encoding="cp1251", errors="ignore") as file:
+        reader = csv.reader(file, delimiter=";", quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             url.append(row[0])
     return url
 
 
-def get_soup(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+def get_soup(url, cookies = None, headers = None, session=None):
+    if not session == None:
+        response = session.get(url, cookies=cookies, headers=headers)
+    else:
+        response = requests.get(url)
+    print(response.status_code)
+    soup = BeautifulSoup(response.text, "lxml")
     return soup
 
 
 def record_to_csv(data):
-    with open('output.csv', 'a', encoding='cp1251', newline='') as file_w:
-        writer = csv.writer(file_w, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    with open("output.csv", "a", encoding="cp1251", newline="") as file_w:
+        writer = csv.writer(file_w, delimiter=";", quoting=csv.QUOTE_MINIMAL)
         writer.writerows([data])
-    print('RECORD COMLETE!')
+    print("RECORD COMLETE!")
 
 
 def send_telegram(text: str):
-    TOKEN = config.TOKEN
+    TOKEN = ConfigGeneral.TOKEN
     url = "https://api.telegram.org/bot"
-    channel_id = config.channel_id
-    url += TOKEN
+    channel_id = ConfigGeneral.CHANNEL_ID
+    url += ConfigGeneral.TOKEN
     method = url + "/sendMessage"
 
-    r = requests.post(method, data={
-        "chat_id": channel_id,
-        "text": text
-    })
+    r = requests.post(method, data={"chat_id": channel_id, "text": text})
 
     if r.status_code != 200:
         raise Exception("post_text error")
 
+
 def stop_list(title):
-    if config.break_word == []:
-        return None
-    for stop_word in config.break_word:
+    if ConfigFilter.BREAK_WORD == []:
+        raise 'No break word in config file!!!! Check it!'
+    for stop_word in ConfigFilter.BREAK_WORD:
         if stop_word.lower() in title.text.lower():
-            print('Титле в стоп листе!!!!')
+            print("Титле в стоп листе!!!!")
             print(stop_word, "============= Стоп ворд")
-            print(title.text, '============= Запрещенный титле')
+            print(title.text, "============= Запрещенный титле")
             return True
+
 
 def time_intrvals(n):
     write, flush = sys.stdout.write, sys.stdout.flush
     for i in reversed(range(n)):
         write(str(i))
         flush()
-        write('\x08' * 10)
+        write("\x08" * 10)
         time.sleep(0.5)
